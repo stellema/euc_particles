a506972b488073ed5caa9f36c1ced34afbdbad24
Annette Stellema <as3189@vdi-n4.novalocal>
2019-09-10 09:51:49 +1000

    subsetting future data and 10 year run finished

--- shell_scripts/subsetu.sh
+++ shell_scripts/subsetu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /g/data/e14/as3189/OFAM/hist/
+cd /g/data3/hh5/tmp/as3189/OFAM/
 
 for f in ocean_u*.nc; do
 	echo "$f"
-	ncks -O -d yu_ocean,-15.0,14.9 -d xu_ocean,120.0,294.9 "${f}" "../trop_pac/${f}";
+	ncks -O -d yu_ocean,-15.0,14.9 -d xu_ocean,120.0,294.9 "${f}" "/g/data/e14/as3189/OFAM/trop_pac/${f}";
 done
