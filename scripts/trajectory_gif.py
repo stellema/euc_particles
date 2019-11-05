cc96bae7b6d9d28d048238169999b3b649da6e27
stellema <40450353+stellema@users.noreply.github.com>
2019-11-06 02:59:24 +1100

    finished simple trajectory video

--- scripts/trajectory_gif.py
+++ scripts/trajectory_gif.py
@@ -7,67 +7,98 @@ author: Annette Stellema (astellemas@gmail.com)
     
 """
 import os
-from pathlib import Path
-os.environ["PROJ_LIB"] = str(Path.home().joinpath('Anaconda3', 'envs', 'TEST', 
-          'Library', 'share'))
+import sys
 import numpy as np
 import xarray as xr
-from netCDF4 import Dataset
-from matplotlib import colors
+from pathlib import Path
 import matplotlib.pyplot as plt
-from main import paths, im_ext, lx
-import matplotlib.gridspec as gridspec
-from mpl_toolkits.basemap import Basemap
-#from AnaObjects import ParticleData, oceanvector, regions
-import matplotlib.animation as animation
-fpath, dpath, xpath = paths()
-#load particle data and get distribution
-dpi = 100
-pdir = str(dpath) + '\\'
-filename = 'ParticleFile_1979-1989_v3'
-pfile=dpath.joinpath('ParticleFile_1979-1989_v3.nc')
-ds = xr.open_dataset(pdir + filename + '.nc')
-
-#ds = ds.reindex(obs=ds.obs[::-1])
-fig = plt.figure(figsize=(15, 5)) #(WIDTH, HEIGHT)
-
-m = Basemap(projection='merc', llcrnrlat=-16, urcrnrlat=16,\
-            llcrnrlon=112, urcrnrlon=235, lat_ts=30, resolution='c')
-
-m.drawparallels([-15, 0, 15], labels=[1, 0, 0, 1], linewidth=1.25, size=13)
-m.drawmeridians([120, 150, 180, -150, -120, -90], labels=[0, 0, 0, 1], 
-                linewidth=1.25, size=13)
-m.drawcoastlines(linewidth=1.75)
-m.fillcontinents(color='dimgrey')
-m.drawmapboundary(fill_color='powderblue')
-
-tj = 1000
-times = range(0, 300)
-x = ds.lon
-y = ds.lat
-
-#lon = np.linspace(165, 165)
-#lat = np.linspace(-2.4, 2.4)
-#X, Y = m(lon, lat)
-#m.plot(X, Y, linewidth=1, color='k', zorder=14)
-
-#colors =  plt.cm.BuPu(np.linspace(0, 1, 600))
-a, b = m(x[tj:, times[0]].values, y[tj:, times[0]].values) 
-graph = m.scatter(a, b, s=3, c='blue')
-
-def animate(obs):
-
-    a, b = m(x[tj:, obs].values, y[tj:, obs].values) 
-    graph.set_offsets(np.c_[a, b])
+from main import paths, im_ext, lx, timeit
+try:
+    if sys.platform == 'darwin' and sys.version_info[0] == 3:
+        import matplotlib
+        matplotlib.use("TkAgg")
+    import matplotlib.animation as animation
+except:
+    anim = None
     
-    return graph,
+os.environ["PROJ_LIB"] = str(Path.home().joinpath('Anaconda3', 
+          'envs', 'TEST', 'Library', 'share'))
+from mpl_toolkits.basemap import Basemap
 
-plt.tight_layout()
-ani = animation.FuncAnimation(fig, animate, times, 
-                              interval=50, blit=False, repeat=True)
-writer = animation.writers['ffmpeg'](fps=30)
+fpath, dpath, xpath = paths()
+filename = dpath.joinpath('ParticleFile_1979-1989_v3.nc')
 
-ani.save(str(fpath.joinpath('demo_traj_{}_{}.mp4'.format(tj, times[0]))), 
-         writer=writer, dpi=dpi)
+#load particle data and get distribution
+@timeit
+def plot_particle_movie(filename, movie_forward=False):
+    os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+    
+    recordedvar = None
+    writer = animation.writers['ffmpeg'](fps=50)
+    try:
+        ds = xr.open_dataset(str(filename), decode_cf=True)
+    except:
+        ds = xr.open_dataset(str(filename), decode_cf=False)
+    lon = np.ma.filled(ds.variables['lon'], np.nan)
+    lat = np.ma.filled(ds.variables['lat'], np.nan)
+    time = np.ma.filled(ds.variables['time'], np.nan) 
+    
+    if(recordedvar is not None):
+        # This is not implemented yet.
+        record = ds.variables[recordedvar]
+    titlestr = ''
+    #plottimes = np.unique(time)
+    plottimes =  np.arange(np.datetime64('1979-01-04T12:00:00.000000000'), 
+                           np.datetime64('1990-01-01T12:00:00.000000000'), 
+                           np.timedelta64(86400000000000,'ns'))
+    if not movie_forward:
+        plottimes = np.flip(plottimes, 0)
+    if isinstance(plottimes[0], (np.datetime64, np.timedelta64)):
+        plottimes = plottimes[~np.isnat(plottimes)]
+    else:
+        try:
+            plottimes = plottimes[~np.isnan(plottimes)]
+        except:
+            pass
+    
+    fig = plt.figure(figsize=(15, 5)) #(WIDTH, HEIGHT)
+    m = Basemap(projection='merc', llcrnrlat=-16, urcrnrlat=16,\
+                llcrnrlon=112, urcrnrlon=235, lat_ts=30, resolution='c')
+    
+    m.drawparallels([-15, 0, 15], labels=[1, 0, 0, 1], linewidth=1.25, size=13)
+    m.drawmeridians([120, 150, 180, -150, -120, -90], labels=[0, 0, 0, 1], 
+                    linewidth=1.25, size=13)
+    m.drawcoastlines(linewidth=1.75)
+    m.fillcontinents(color='dimgrey')
+    m.drawmapboundary(fill_color='powderblue')
 
-plt.show()
+    x, y = m(np.linspace(165, 165), np.linspace(-2.6, 2.6))
+    m.plot(x, y, linewidth=4, color='k', zorder=14)
+    
+    t = 0
+    b = time == plottimes[t]
+    X, Y = m(lon[b], lat[b]) 
+    graph = m.scatter(X, Y, s=6, marker='o', c='blue')
+    ttl = plt.title('Particles' + titlestr + ' at time ' + str(plottimes[t])[:10])
+    
+    def animate(t, graph):
+        b = plottimes[t] == time
+        X, Y = m(lon[b], lat[b]) 
+        graph.set_offsets(np.c_[X, Y])
+    
+        if recordedvar is not None:
+            # Not implemeneted yet.
+            graph.set_array(record[b])
+        ttl.set_text('Particle' + titlestr + ' at time ' + str(plottimes[t])[:10])
+        fig.canvas.draw()
+        return graph,
+    
+    frames = np.arange(1, len(plottimes))
+    plt.rc('animation', html='html5')
+    anim = animation.FuncAnimation(fig, animate, fargs=(graph,),
+                                   frames=frames, interval=75, 
+                                   blit=False, repeat=True)
+    plt.close()
+    anim.save(str(fpath.joinpath('demo_traj_blue_black.mp4')), writer=writer)
+
+plot_particle_movie(filename, movie_forward=False)
