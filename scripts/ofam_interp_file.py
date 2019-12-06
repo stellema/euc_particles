4640b70c586ae25613883eea5372426094912cd9
Annette Stellema <astellemas@gmail.com>
2019-12-06 13:03:01 +1100

    still working on interp comp

---
+++ scripts/ofam_interp_file.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+created: Thu Dec  5 03:26:22 2019
+
+author: Annette Stellema (astellemas@gmail.com)
+
+    
+"""
+import sys
+sys.path.append('/g/data1a/e14/as3189/OFAM/scripts/')
+import numpy as np
+import xarray as xr
+from pathlib import Path
+from main import paths, LAT_DEG, lx
+
+
+# Path to save figures, save data and OFAM model output.
+fpath, dpath, xpath, lpath = paths()
+tpath = Path('/g', 'data', 'e14', 'as3189', 'OFAM', 'TAO')
+lons = [165, 190, 220]
+# Area = metres in a degree of latitude x cell width x cell depth
+area = LAT_DEG*0.1*5
+
+files = []
+for y in range(lx['years'][0][0], lx['years'][0][1]+1):
+    for m in range(1, 13):
+        files.append(str(xpath.joinpath('ocean_u_{}_{:02d}.nc'.format(y, m))))
+        
+ds = xr.open_mfdataset(files, combine='by_coords')
+dss = ds.u.sel(xu_ocean=[165, 190, 220], yu_ocean=slice(-2.6, 2.6), 
+              st_ocean=slice(2.5, 610.415649))
+# Calculate the monthly mean.
+df = dss.resample(Time="MS").mean()
+# New depth levels to interpolate to.
+z = np.arange(10, 360, 5)
+di = df.interp(st_ocean=z, method='slinear')
+
+di = di.sel(st_ocean=slice(10, 555))
+# Multiply each grid cell by the constant.
+dt = (di*area).sum(dim='yu_ocean')
+
+eq = di.sel(yu_ocean=0, method='nearest')
+
+dz = xr.Dataset()
+dz['uvo'] = dt
+dz['uvo']['long_name'] = 'OFAM3 EUC monthly zonal transport slinear with interpolated depth levels'
+dz['uvo']['units'] = 'm3/sec'
+dz['u'] = eq
+dz['u']['long_name'] = 'OFAM3 EUC monthly zonal velocity at the equator with slinear interpolated depth levels'
+dz['u']['units'] = 'm/sec'
+
+# Save to /data as a netcdf file.
+dz.to_netcdf(dpath.joinpath('ofam_EUC_int_transport.nc'))
