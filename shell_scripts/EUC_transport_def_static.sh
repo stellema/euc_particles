89f6528d4d54ef2799d7a2ff69f2e46016d1347f
Annette Stellema <astellemas@gmail.com>
2020-03-19 15:42:08 +1100

    qsubs of hist create_file_ EUC_transport_definitions

---
+++ shell_scripts/EUC_transport_def_static.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normal
+#PBS -l walltime=2:00:00
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -M astellemas@gmail.com
+#PBS -m abe
+#PBS -l storage=gdata/hh5+gdata/e14
+
+module use /g/data3/hh5/public/modules
+module load conda/analysis3-20.01
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_EUC_transport_definitions.py 'static' 0
