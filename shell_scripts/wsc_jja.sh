0c495c5135e9b6e3ff351c489790587d2ab24ab1
Annette Stellema <as3189@gadi-login-03.gadi.nci.org.au>
2020-04-08 17:38:33 +1000

    Misc. gadi

--- shell_scripts/wsc_jja.sh
+++ shell_scripts/wsc_jja.sh
@@ -7,9 +7,9 @@
 #PBS -l wd
 #PBS -M astellemas@gmail.com
 #PBS -m abe
-#PBS -l storage=gdata/hh5+gdata/e14+gdata/ua8
+#PBS -l storage=gdata/hh5+gdata/e14+gdata/rr7
 
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.01
 
-python3 /g/data/e14/as3189/OFAM/scripts/create_file_wsc.py 'jra55-do'
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_wsc.py 'jra55'
