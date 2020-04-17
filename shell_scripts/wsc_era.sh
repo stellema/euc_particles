250c70f13521f793b2cc62d73d21bd5be27d7df8
stellema <40450353+stellema@users.noreply.github.com>
2020-04-18 09:28:17 +1000

    Update wsc_era.sh

--- shell_scripts/wsc_era.sh
+++ shell_scripts/wsc_era.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=10:00:00
-#PBS -l mem=30GB
-#PBS -l ncpus=24
+#PBS -l walltime=20:00:00
+#PBS -l mem=15GB
+#PBS -l ncpus=6
 #PBS -l wd
 #PBS -M astellemas@gmail.com
 #PBS -m abe
@@ -11,10 +11,11 @@
 
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.01
-for r in 0.1 0.5 1 1.5; do
-for i in 0 1 2 3 4 5; do
-	python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' $i $r &
-done
-done
 
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' 0 0.5 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' 1 0.5 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' 2 0.1 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' 2 0.5 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' 4 0.1 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' 4 0.5 &
 wait
