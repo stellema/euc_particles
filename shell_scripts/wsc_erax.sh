0e6db444ba9522b90dc1e54373db13694e765a35
stellema <40450353+stellema@users.noreply.github.com>
2020-04-18 18:26:28 +1000

    Misc

---
+++ shell_scripts/wsc_erax.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normal
+#PBS -l walltime=20:00:00
+#PBS -l mem=15GB
+#PBS -l ncpus=6
+#PBS -l wd
+#PBS -M astellemas@gmail.com
+#PBS -m abe
+#PBS -l storage=gdata/hh5+gdata/e14+gdata/rr7
+
+module use /g/data3/hh5/public/modules
+module load conda/analysis3-20.01
+
+for i in 0 1 2 3 4 5; do
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'erai' $i 1 &
+done
+wait
+
