f81e1342b794424c219d2576039665d7138425f4
stellema <40450353+stellema@users.noreply.github.com>
2020-04-18 11:35:34 +1000

    Update wsc_jra.sh

--- shell_scripts/wsc_jra.sh
+++ shell_scripts/wsc_jra.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=15:00:00
-#PBS -l mem=30GB
-#PBS -l ncpus=20
+#PBS -l walltime=23:00:00
+#PBS -l mem=20GB
+#PBS -l ncpus=11
 #PBS -l wd
 #PBS -M astellemas@gmail.com
 #PBS -m abe
@@ -12,10 +12,17 @@
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.01
 
-for r in 0.1 0.5 1 1.5; do
-for i in 0 1 2 3 4; do
+for r in 0.5; do
+for i in 0 1 2 4; do
 	python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' $i $r &
 done
 done
 
+for k in 0 2 3 4; do
+	python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' $k 1.5 &
+done
+
+for j in 1 2 4; do
+	python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' $j 1 &
+done
 wait
