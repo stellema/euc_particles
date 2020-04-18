1d92b6e9921daf52cf129646886a9f317b126510
stellema <40450353+stellema@users.noreply.github.com>
2020-04-18 14:55:47 +1000

    Shell script mods

---
+++ shell_scripts/wsc_jrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normal
+#PBS -l walltime=23:00:00
+#PBS -l mem=20GB
+#PBS -l ncpus=5
+#PBS -l wd
+#PBS -M astellemas@gmail.com
+#PBS -m abe
+#PBS -l storage=gdata/hh5+gdata/e14+gdata/rr7
+
+module use /g/data3/hh5/public/modules
+module load conda/analysis3-20.01
+
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' 0 0.5 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' 1 0.5 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' 1 1 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' 2 1.5 &
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_reanalysis_wind.py 'jra55' 4 1.5 &
+
+wait
