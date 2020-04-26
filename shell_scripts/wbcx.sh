3b375c2667102bf060561a3979ab1a6cb35940a2
Annette Stellema <as3189@gadi-login-04.gadi.nci.org.au>
2020-04-26 15:34:21 +1000

    Gadi

--- shell_scripts/wbcx.sh
+++ shell_scripts/wbcx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=10:00:00
-#PBS -l mem=25GB
-#PBS -l ncpus=1
+#PBS -l walltime=24:00:00
+#PBS -l mem=50GB
+#PBS -l ncpus=5
 #PBS -l wd
 #PBS -M astellemas@gmail.com
 #PBS -m abe
@@ -11,5 +11,7 @@
 
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.01
-
-python3 /g/data/e14/as3189/OFAM/scripts/create_file_ofam_llwbc.py 0
+for i in 0 1 2 3 4; do
+python3 /g/data/e14/as3189/OFAM/scripts/create_file_ofam_llwbc.py $i &
+done
+wait
