161c4ac1fd6bbb3dbb6e8d51e8df6876068b7b26
stellema <40450353+stellema@users.noreply.github.com>
2020-04-27 11:05:45 +1000

    Deleted error files.

--- scripts/transport_log.py
+++ scripts/transport_log.py
@@ -11,30 +11,50 @@ import logging
 import numpy as np
 import xarray as xr
 from datetime import datetime
-from main import paths, lx, SV, mlogger
+from main import paths, lx, SV, mlogger, idx
 from valid_nino34 import enso_u_ofam, nino_events
-
 
 # Path to save figures, save data and OFAM model output.
 fpath, dpath, xpath, lpath, tpath = paths()
 
 logger = mlogger('transport_log')
 
-
 oni = xr.open_dataset(dpath/'ofam_sst_anom_nino34_hist.nc')
-euc = xr.open_dataset(dpath/'ofam_EUC_transport_static_hist.nc').uvo
+euc = xr.open_dataset(dpath/'ofam_EUC_transport_static_hist.nc').uvo/SV
+
+vs = xr.open_dataset(dpath/'ofam_transport_vsz.nc').vvo/SV
+mc = xr.open_dataset(dpath/'ofam_transport_mcz.nc').vvo/SV
+ss = xr.open_dataset(dpath/'ofam_transport_ssz.nc').vvo/SV
+sg = xr.open_dataset(dpath/'ofam_transport_sgz.nc').vvo/SV
+
+vs = vs.isel(st_ocean=slice(0, idx(vs.st_ocean, 1000) + 2))
+vs = vs.sum(dim='st_ocean').sum(dim='xu_ocean')
+mc = mc.isel(st_ocean=slice(0, idx(mc.st_ocean, 600) + 1))
+mc = mc.sum(dim='st_ocean').sum(dim='xu_ocean')
+
+sg = sg.isel(st_ocean=slice(0, idx(sg.st_ocean, 1200) + 1))
+sg = sg.sum(dim='st_ocean').sum(dim='xu_ocean')
+
+ss = ss.isel(st_ocean=slice(0, idx(ss.st_ocean, 1200) + 1))
+ss = ss.sum(dim='st_ocean').sum(dim='xu_ocean')
 # nino, nina = nino_events(oni.oni)
 
 # DataArrays and names.
-data = [euc.isel(xu_ocean=i)/SV for i in range(3)]
-name = ['EUC at {}'.format(i) for i in lx['lonstr']]
+data = [*[euc.isel(xu_ocean=i) for i in range(3)],
+        vs, sg, ss,
+        *[mc.isel(yu_ocean=i) for i in [-1, 0]]]
+name = [*['EUC at {}'.format(i) for i in lx['lonstr']], 'Vitiaz Strait',
+        'St. George', 'Solomon ',
+        'MC at 9N', 'MC at 6.4N']
 
 # Log mean, seasonal and interannual transports.
 for nm, ds in zip(name, data):
     enso_mod = enso_u_ofam(oni, ds).values.tolist()
     s = ds.groupby('Time.month').mean().rename({'month': 'Time'}).values
     mx, mn = np.argmax(s), np.argmin(s)
-    logger.info('{}: Mean: {:.1f} Sv, Max: {:.1f} Sv in {}, Min: {:.1f} Sv '
-                'in {}, El Nino: {:.1f} Sv, La Nina: {:.1f} Sv.'
+    if s.mean() <= 0:
+        mx, mn = mn, mx
+    logger.info('{:<13s}: Mean: {: <2.1f} Sv, Max: {: <2.1f} Sv in {}, Min: '
+                '{: .1f} Sv in {}, El Nino: {: .1f} Sv, La Nina: {: <2.1f} Sv.'
                 .format(nm, s.mean(), s[mx], lx['mon'][mx], s[mn],
                         lx['mon'][mn], *enso_mod))
