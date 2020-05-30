bacacd7338e5fdd84042904571ea9bfe186b7c94
Annette Stellema <as3189@gadi-login-04.gadi.nci.org.au>
2020-05-30 19:15:37 +1000

    misc.

--- jobs/base14x6.sh
+++ jobs/base14x6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=24:00:00
-#PBS -l mem=100GB
+#PBS -l walltime=48:00:00
+#PBS -l mem=190GB
 #PBS -l ncpus=14
 #PBS -l wd
 #PBS -M astellemas@gmail.com
@@ -10,4 +10,4 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
-mpirun -np 14 python3 /g/data/e14/as3189/OFAM/scripts/base.py -dy 0.1 -dz 25 -lon "165" -i 2012 -f 2012 -p True -ix 1426 -m2 12 -m1 6
+mpirun -np 14 python3 /g/data/e14/as3189/OFAM/scripts/base.py -dy 0.1 -dz 25 -lon "165" -i 2012 -f 2012 -p True -ix 41 -m2 12 -m1 7
