159c17fa7eb23cb952072af8cff4b1675c49bb79
stellema <40450353+stellema@users.noreply.github.com>
2020-06-09 17:36:43 +1000

    Major base changes

--- scripts/base.py
+++ scripts/base.py
@@ -11,27 +11,39 @@
 # qsub -I -l walltime=5:00:00,mem=400GB,ncpus=7 -P e14 -q hugemem -X -l wd
 # qsub -I -l walltime=7:00:00,ncpus=1,mem=20GB -P e14 -q normal -l wd
 
+# if partitions is not None:
+#     mpi_size = 26
+#     p = np.arange(0, mpi_size, dtype=int)
+#     partitions = np.append(np.repeat(p, 28),  np.ones(14, dtype=int)*(mpi_size-1))
 # """
 import main
 import cfg
 import tools
 import math
+import shutil
 import numpy as np
 from pathlib import Path
 from tools import get_date, timeit
-from datetime import timedelta
+from datetime import datetime, timedelta
 from argparse import ArgumentParser
 from parcels import (FieldSet, Field, ParticleSet, JITParticle,
                      ErrorCode, Variable, AdvectionRK4_3D, AdvectionRK4)
+
+try:
+    from mpi4py import MPI
+    from sklearn.cluster import KMeans
+
+except:
+    MPI = None
+
 
 logger = tools.mlogger('base', parcels=True)
 
 
 @timeit
 def run_EUC(dy=0.1, dz=25, lon=165, lat=2.6, year=2012, month=12, day='max',
-            dt_mins=60, repeatdt_days=6, outputdt_days=1, runtime_days=185, ifile=0,
-            field_method='b_grid', chunks='specific', partition=None,
-            pfile='', parallel=False):
+            dt_mins=60, repeatdt_days=6, outputdt_days=1, runtime_days=185, v=0,
+            chunks=300, pfile='None', parallel=False, interp_method='linear_invdist_land_tracer'):
     """Run Lagrangian EUC particle experiment."""
     # Define Fieldset and ParticleSet parameters.
 
@@ -43,87 +55,70 @@ def run_EUC(dy=0.1, dz=25, lon=165, lat=2.6, year=2012, month=12, day='max',
     # Number of particles released in each dimension.
     Z, Y, X = len(p_depths), len(p_lats), len(p_lons)
 
-    # Make sure run ends on a repeat day, otherwise increase until it does.
-    # while runtime_days % repeatdt_days != 0:
-    #     runtime_days += 1
+    # Make sure run ends before a repeat day, otherwise increase until it does.
+    while runtime_days % repeatdt_days != (repeatdt_days - 1):
+        runtime_days += 1
     runtime = timedelta(days=int(runtime_days))
-
-    # Start and end dates.
-    start_date = get_date(year, month, day)
-    date_bnds = [start_date - runtime, start_date]
 
     dt = -timedelta(minutes=dt_mins)  # Advection step (negative for backwards).
     repeatdt = timedelta(days=repeatdt_days)  # Repeat particle release time.
     outputdt = timedelta(days=outputdt_days)  # Advection steps to write.
 
-    # Fieldset bounds.
-    y2 = 2012 if cfg.home != Path('E:/') else 1981
-    ft_bnds = [get_date(1981, 1, 1), get_date(y2, 12, 31)]
-    time_periodic = timedelta(days=(ft_bnds[1] - ft_bnds[0]).days + 1)
-
-    if partition == 'specific':
-        mpi_size = 26
-        p = np.arange(0, mpi_size, dtype=int)
-        partitions = np.append(np.repeat(p, 28),  np.ones(14, dtype=int)*(mpi_size-1))
-    else:
-        partitions = None
-
-    # Generate file name for experiment.
-    sim_id = main.generate_sim_id(date_bnds, lon, ifile=ifile, parallel=parallel)
-    sim = sim_id.stem if pfile == '' else (cfg.data/pfile).stem
-
-    logger.info('{}:Executing:{} to {}: Runtime={} days: Particles: /repeat={}: Total={}'
-                .format(sim, *[str(i)[:10] for i in date_bnds], runtime.days,
-                        Z * X * Y, Z * X * Y * math.ceil(runtime.days/repeatdt.days)))
-    logger.info('{}:Lons={}: Lats={} [{}-{} x{}]: Depths={} [{}-{}m x{}]'
-                .format(sim, *p_lons, Y, *p_lats[::Y-1], dy, Z, *p_depths[::Z-1], dz))
-    logger.info('{}:Repeat={} days: Step={:.0f} mins: Output={:.0f} day'
-                .format(sim, repeatdt.days, 1440 - dt.seconds/60, outputdt.days))
-    logger.info('{}:Field={}: Chunks={}300: Range={}-{}: invdist'
-                .format(sim, field_method, chunks, ft_bnds[0].year, ft_bnds[1].year))
-
     # Create fieldset.
-    fieldset = main.ofam_fieldset(ft_bnds, chunks=chunks, field_method=field_method,
-                                  time_periodic=time_periodic)
+    y2 = 2012 if cfg.home != Path('E:/') else 1981
+    time_bnds = [datetime(1981, 1, 1), datetime(y2, 12, 31)]
+    fieldset = main.ofam_fieldset(time_bnds, chunks=300, time_periodic=True,
+                                  interp_method=interp_method)
 
-    class zParticle(cfg.ptype['jit']):
-        """Particle class that saves particle age and zonal velocity."""
-
-        # The age of the particle.
-        age = Variable('age', dtype=np.float32, initial=0.)
-
-        # The velocity of the particle.
-        u = Variable('u', dtype=np.float32, initial=fieldset.U, to_write='once')
-
-        # The 'zone' of the particle.
-        zone = Variable('zone', dtype=np.float32, initial=fieldset.zone)
+    zParticle = main.get_zParticle(fieldset)
 
     # Create particle set.
-    if pfile == '':
+    if pfile == 'None':
+        # Generate file name for experiment.
+        sim_id = main.generate_sim_id(lon, v, parallel=parallel)
+
         # Set ParticleSet start as last fieldset time.
         pset_start = fieldset.U.grid.time[-1]
-        pset = main.EUC_pset(fieldset, zParticle, p_lats, p_lons, p_depths, pset_start,
-                             repeatdt, partitions)
-        # Output particle file p_name and time steps to save.
-        output_file = pset.ParticleFile(cfg.data/sim_id.stem, outputdt=outputdt)
+        pset = main.EUC_pset(fieldset, zParticle, p_lats, p_lons, p_depths, pset_start, repeatdt)
+        start = get_date(year, month, day)
+        end = start - runtime
     # Create particle set from particlefile.
     else:
+        sim = cfg.data/pfile
+        sim_id = cfg.data/(sim.stem[:-1] + str(int(sim.stem[-1]) + 1) + '.nc')
         pfile = cfg.data/pfile
+
         pset = main.particleset_from_particlefile(fieldset, pclass=zParticle, filename=pfile,
                                                   restart=True, restarttime=np.nanmin)
         pset_start = np.nanmin(pset.time) - 24*60*60
         # if (pset.age[pset.time == pset_start] == 0.).sum() == Z * X * Y:
         #     pset_start = pset_start - repeatdt_days*24*60*60
-        logger.info('{}:Starting from {}: start={}: fsetstart={}'
-                    .format(sim, pfile.stem, pset_start, fieldset.U.grid.time[-1]))
+
+        start = fieldset.time_origin.time_origin + timedelta(seconds=np.nanmin(pset.time))
+        end = start - runtime
+
         psetx = main.EUC_pset(fieldset, zParticle, p_lats, p_lons, p_depths, pset_start, repeatdt)
         pset.add(psetx)
+
         # Output particle file p_name and time steps to save.
         output_file = pset.ParticleFile(pfile, outputdt=outputdt)
 
-    logger.debug('{}: dir={}: #Particles={}'.format(sim, output_file.tempwritedir_base[-8:],
-                                                    pset.size))
-
+    # Output particle file p_name and time steps to save.
+    output_file = pset.ParticleFile(cfg.data/sim_id.stem, outputdt=outputdt)
+    if MPI:
+        rank = MPI.COMM_WORLD.Get_rank()
+        if rank == 1:
+            logger.info('{}:{}-{}: Runtime={} days'.format(sim_id.stem, start, end, runtime.days))
+            logger.info('{}:Particles: /repeat={}: Total={}'
+                        .format(sim_id.stem, Z * X * Y, Z * X * Y * math.ceil(runtime.days/repeatdt.days)))
+            logger.info('{}:Lon={}: Lat=[{}-{} x{}]: Depth=[{}-{}m x{}]'
+                        .format(sim_id.stem, *p_lons, *p_lats[::Y-1], dy, *p_depths[::Z-1], dz))
+            logger.info('{}:Repeat={} days: Step={:.0f} mins: Output={:.0f} day'
+                        .format(sim_id.stem, repeatdt.days, 1440 - dt.seconds/60, outputdt.days))
+            logger.info('{}:Chunks={}: Time={}-{}: Interp={}'
+                        .format(sim_id.stem, chunks, time_bnds[0].year, time_bnds[1].year, interp_method))
+        logger.info('{}: Temp={}: Rank={}: #Particles={}'
+                    .format(sim_id.stem, output_file.tempwritedir_base[-8:], rank, pset.size))
     kernels = pset.Kernel(main.Age) + AdvectionRK4_3D
 
     pset.execute(kernels, runtime=runtime, dt=dt, output_file=output_file,
@@ -149,23 +144,22 @@ if __name__ == "__main__" and cfg.home != Path('E:/'):
     p.add_argument('-r', '--repeatdt', default=6, type=int, help='Release repeat [day].')
     p.add_argument('-out', '--outputdt', default=1, type=int, help='Advection write freq [day].')
     p.add_argument('-p', '--parallel', default=False, type=bool, help='Parallel execution.')
-    p.add_argument('-ix', '--ifile', default=0, type=int, help='File Index.')
-    p.add_argument('-f', '--pfile', default='', type=str, help='Particle file.')
+    p.add_argument('-ix', '--v', default=0, type=int, help='File Index.')
+    p.add_argument('-f', '--pfile', default='None', type=str, help='Particle file.')
     args = p.parse_args()
 
     run_EUC(dy=args.dy, dz=args.dz, lon=args.lon, lat=args.lat, year=args.year, month=args.month,
             runtime_days=args.runtime, dt_mins=args.dt, repeatdt_days=args.repeatdt,
-            outputdt_days=args.outputdt, ifile=args.ifile, parallel=args.parallel, pfile=args.pfile)
+            outputdt_days=args.outputdt, v=args.v, parallel=args.parallel, pfile=args.pfile)
 else:
     dy, dz = 2, 200
     lon, lat = 165, 2.6
     year, month, day = 1981, 1, 'max'
     dt_mins, repeatdt_days, outputdt_days, runtime_days = 240, 6, 1, 6
-    field_method = 'b_grid'
-    chunks = 'specific'
-    partition = None
+    chunks = 300
     pfile = 'sim_201206_201212_165_v8c.nc'
-    ifile = 0
+    v = 0
+    interp_method='linear_invdist_land_tracer'
     run_EUC(dy=dy, dz=dz, lon=lon, lat=lat, year=year,
             dt_mins=240, repeatdt_days=6, outputdt_days=1, month=month,
-            runtime_days=runtime_days, field_method=field_method, chunks=chunks, partition=partition)
+            runtime_days=runtime_days, chunks=chunks)
--- scripts/main.py
+++ scripts/main.py
@@ -63,8 +63,9 @@ from operator import attrgetter
 logger = tools.mlogger(Path(sys.argv[0]).stem)
 
 
-def ofam_fieldset(date_bnds, field_method='b_grid', chunks='specific', cs=300,
-                  time_periodic=False, deferred_load=True, time_ext=False):
+def ofam_fieldset(time_bnds='full', chunks=300, time_periodic=True,
+                  deferred_load=True, time_ext=False,
+                  interp_method='linear_invdist_land_tracer'):
     """Create a 3D parcels fieldset from OFAM model output.
 
     Between two dates useing FieldSet.from_b_grid_dataset.
@@ -87,40 +88,36 @@ def ofam_fieldset(date_bnds, field_method='b_grid', chunks='specific', cs=300,
                                                  "depth": ["st_ocean", "sw_ocean"],
                                                  "time": ["time"]}
 
+    if time_bnds == 'full':
+        y2 = 2012 if cfg.home != Path('E:/') else 1981
+        time_bnds = [datetime(1981, 1, 1), datetime(y2, 12, 31)]
+
+    if time_periodic is True:
+        time_periodic = timedelta(days=(time_bnds[1] - time_bnds[0]).days + 1)
+
     # Create list of files for each variable based on selected years and months.
     u, v, w = [], [], []
-    for y in range(date_bnds[0].year, date_bnds[1].year + 1):
-        for m in range(date_bnds[0].month, date_bnds[1].month + 1):
+    for y in range(time_bnds[0].year, time_bnds[1].year + 1):
+        for m in range(time_bnds[0].month, time_bnds[1].month + 1):
             u.append(str(cfg.ofam/('ocean_u_{}_{:02d}.nc'.format(y, m))))
             v.append(str(cfg.ofam/('ocean_v_{}_{:02d}.nc'.format(y, m))))
             w.append(str(cfg.ofam/('ocean_w_{}_{:02d}.nc'.format(y, m))))
 
     variables = {'U': 'u', 'V': 'v', 'W': 'w'}
-    dimensions = {'time': 'Time', 'depth': 'st_ocean',
-                  'lat': 'yu_ocean', 'lon': 'xu_ocean'}
+    dimensions = {'time': 'Time', 'depth': 'st_ocean', 'lat': 'yu_ocean', 'lon': 'xu_ocean'}
 
     files = {'U': {'depth': u[0], 'lat': u[0], 'lon': u[0], 'data': u},
              'V': {'depth': u[0], 'lat': u[0], 'lon': u[0], 'data': v},
              'W': {'depth': u[0], 'lat': u[0], 'lon': u[0], 'data': w}}
 
-    if chunks == 'specific':
+    if chunks not in ['auto', False]:
         chunks = {'Time': 1, 'st_ocean': 1, 'sw_ocean': 1,
-                  'yt_ocean': cs, 'yu_ocean': cs,
-                  'xt_ocean': cs, 'xu_ocean': cs}
+                  'yt_ocean': chunks, 'yu_ocean': chunks,
+                  'xt_ocean': chunks, 'xu_ocean': chunks}
 
-    if field_method == 'b_grid':
-        fieldset = FieldSet.from_b_grid_dataset(files, variables, dimensions, mesh='spherical',
-                                                field_chunksize=chunks, time_periodic=time_periodic,
-                                                allow_time_extrapolation=time_ext)
-    elif field_method == 'netcdf':
-        fieldset = FieldSet.from_netcdf(files, variables, dimensions, time_periodic=time_periodic,
-                                        field_chunksize=chunks, allow_time_extrapolation=time_ext)
-
-    elif field_method == 'xarray':
-        ds = xr.open_mfdataset(u + v + w, combine='by_coords', concat_dim='Time')
-        fieldset = FieldSet.from_xarray_dataset(ds, variables, dimensions, mesh='spherical',
-                                                time_periodic=time_periodic, field_chunksize=chunks,
-                                                allow_time_extrapolation=time_ext)
+    fieldset = FieldSet.from_b_grid_dataset(files, variables, dimensions, mesh='spherical',
+                                            field_chunksize=chunks, time_periodic=time_periodic,
+                                            allow_time_extrapolation=time_ext)
 
     # Add EUC boundary zones to fieldset.
     zfield = Field.from_netcdf(str(cfg.data/'OFAM3_zones.nc'), 'zone',
@@ -131,26 +128,25 @@ def ofam_fieldset(date_bnds, field_method='b_grid', chunks='specific', cs=300,
 
     # Set fieldset minimum depth.
     fieldset.mindepth = fieldset.U.depth[0]
-    interp_method = 'linear_invdist_land_tracer'
+    fieldset.interp_method = interp_method
 
     return fieldset
 
 
-def generate_sim_id(date_bnds, lon, ifile=0, parallel=False):
+def generate_sim_id(lon, v=0, parallel=False):
     """Create name to save particle file (looks for unsaved filename)."""
-    # dsr = 'sim_{}_{}_{}'.format(*[str(i)[:7].replace('-', '') for i in date_bnds], int(lon))
     dsr = 'sim_{}'.format(int(lon))
     i = 0 if parallel else random.randint(0, 200)
 
-    while (cfg.data/'{}_v{}i.nc'.format(dsr, i)).exists():
+    while (cfg.data/'{}_v{}r0.nc'.format(dsr, i)).exists():
         i = i + 1 if parallel else random.randint(0, 200)
-    if ifile != 0:
-        if not (cfg.data/'{}_v{}.nc'.format(dsr, ifile)).exists():
-            i = ifile
+    if v != 0:
+        if not (cfg.data/'{}_v{}r0.nc'.format(dsr, v)).exists():
+            i = v
         else:
-            i = ifile + 1
+            i = v + 1
 
-    sim_id = cfg.data/'{}_v{}.nc'.format(dsr, i)
+    sim_id = cfg.data/'{}_v{}r0.nc'.format(dsr, i)
     return sim_id
 
 
@@ -295,6 +291,7 @@ def EUC_pset(fieldset, pclass, p_lats, p_lons, p_depths, pset_start, repeatdt, p
 
 
 def get_zParticle(fieldset):
+    """Get zParticle class."""
     class zParticle(cfg.ptype['jit']):
         """Particle class that saves particle age and zonal velocity."""
 
@@ -483,257 +480,3 @@ def plot3D(sim_id):
 # ax.set_ylabel("Latitude")
 # ax.set_zlabel("Depth [m]")
 # ax.set_zlim(np.max(z), np.min(z))
-
-
-
-##############################################################################
-# VALIDATION
-##############################################################################
-
-
-def EUC_vbounds(du, depths, i, v_bnd=0.3, index=False):
-    """Find EUC max velocity/position and lower EUC depth boundary.
-
-    Args:
-        du (array): Velocity values.
-        depths (array): Depth values.
-        i ({0, 1, 2}): Index of longitude.
-        v_bnd (float, str): Minimum velocity to include. Defaults to 0.3.
-        index (bool, optional): Return depth index or value. Defaults to False.
-
-    Returns:
-        v_max (array): Maximum velocity at each timestep.
-        array: Depth of maximum velocity at each timestep.
-        array: Deepest EUC depth based on v_bnd at each timestep.
-
-    """
-    du = du.where(du >= 0)
-    u = np.ma.masked_invalid(du)
-
-    # Maximum and minimum velocity at each time step.
-    v_max = du.max(axis=1, skipna=True)
-    v_max_half = v_max/2
-    v_max_25 = v_max*0.25
-
-    # Index of maximum and minimum velocity at each time.
-    v_imax = np.nanargmax(u, axis=1)
-
-    z1i = (v_imax.copy()*np.nan)
-    z2i = (v_imax.copy()*np.nan)
-    z1 = v_imax.copy()*np.nan
-    z2 = v_imax.copy()*np.nan
-
-    target = v_bnd if v_bnd == 'half_max' else v_max_half[0]
-
-    count, empty, skip_t, skip_l = 0, 0, 0, 0
-
-    for t in range(u.shape[0]):
-        # Make sure entire slice isn't all empty
-        if ~(u[t] == True).mask.all() and ~np.ma.is_masked(v_imax[t]):
-
-            # Set target velocity as half the maximum at each timestep.
-            if v_bnd == 'half_max':
-                target = v_max_half[t]
-            elif v_bnd == '25_max':
-                target = v_max_25[t]
-
-            # Subset velocity on either side of the maximum velocity.
-            top = du[t, slice(0, v_imax[t])]
-            low = du[t, slice(v_imax[t], len(depths))]
-
-            # Mask velocities that are greater than the maxmimum.
-            top = top.where(top <= target)
-            low = low.where(low <= target)
-
-            # Find the closest velocity depth/index if both the
-            # top and lower arrays are not all NaN.
-            if all([not all(np.isnan(top)), not all(np.isnan(low))]):
-                for k in np.arange(len(top)-1, 0, -1):
-                    if not np.isnan(top[k]):
-                        for j in np.arange(k-1, 0, -1):
-                            if np.isnan(top[j]):
-                                top[0:j] = top[0:j]*np.nan
-                            break
-
-                z1i[t] = tools.idx(top, target)
-                z1[t] = depths[int(z1i[t])]
-                z2i[t] = tools.idx(low, target) + v_imax[t]
-                z2[t] = depths[int(z2i[t])]
-                count += 1
-                if abs(z2[t] - z1[t]) < 50:
-                    z1i[t], z2i[t] = np.nan, np.nan
-                    z1[t], z2[t] = np.nan, np.nan
-                    count -= 1
-                if z2[t] < 125:
-                    z1i[t], z2i[t] = np.nan, np.nan
-                    z1[t], z2[t] = np.nan, np.nan
-                    count -= 1
-
-            # Check if skipped steps due to missing top depth (and vice versa).
-            if all(np.isnan(low)) and not all(np.isnan(top)):
-                skip_t += 1
-            elif all(np.isnan(top)) and not all(np.isnan(low)):
-                skip_l += 1
-        else:
-            empty += 1
-
-    # data_name = 'OFAM3' if hasattr(du, 'st_ocean') else 'TAO/TRITION'
-    # logger.debug('{} {}: v_bnd={} tot={} count={} null={} skip={}(T={},L={}).'
-    #              .format(data_name, cfg.lons[i], v_bnd, u.shape[0], count,
-    #                      empty, skip_t + skip_l, skip_t, skip_l))
-    if not index:
-        return v_max, z1, z2
-    else:
-        return v_max, z1i, z2i
-
-
-def EUC_bnds_static(du, lon=None, z1=25, z2=350, lat=2.6):
-    """Apply static EUC definition to zonal velocity at a longitude.
-
-    Args:
-        du (Dataset): Zonal velocity dataset.
-        lon (float): The EUC longitude examined.
-        z1 (float): First depth level.
-        z2 (float): Final depth level.
-        lat (float): Latitude bounds.
-
-    Returns:
-        du4 (DataArray): The zonal velocity in the EUC region.
-
-    """
-    z1 = tools.get_edge_depth(z1, index=False)
-    z2 = tools.get_edge_depth(z2, index=False)
-
-    # Slice depth and longitude.
-    du = du.sel(st_ocean=slice(z1, z2), yu_ocean=slice(-lat, lat))
-    if lon is not None:
-        du = du.sel(xu_ocean=lon)
-
-    # Remove negative/zero velocities.
-    du = du.u.where(du.u > 0, np.nan)
-
-    return du
-
-
-def EUC_bnds_grenier(du, dt, ds, lon):
-    """Apply Grenier EUC definition to zonal velocity at a longitude.
-
-    Grenier et al. (2011) EUC definition:
-        - Equatorial eastward flow (u > 1 m s−1)
-        - Between σθ = 22.4 kg m−3 to 26.8 kg m−3
-        - Between 2.625°S to 2.625°N
-
-    Args:
-        du (Dataset): Zonal velocity dataset.
-        dt (Dataset): Temperature dataset.
-        ds (Dataset): Salinity dataset.
-        lon (float): The EUC longitude examined.
-
-    Returns:
-        du3 (dataset): The zonal velocity in the EUC region.
-
-    """
-    lat = 2.625
-    rho1 = 22.4
-    rho2 = 26.8
-
-    # Find exact latitude longitudes to slice dt and ds.
-    lat_i = dt.yt_ocean[tools.idx(dt.yt_ocean, -lat + 0.05)].item()
-    lat_f = dt.yt_ocean[tools.idx(dt.yt_ocean, lat + 0.05)].item()
-    lon_i = dt.xt_ocean[tools.idx(dt.xt_ocean, lon + 0.05)].item()
-    du = du.sel(xu_ocean=lon, yu_ocean=slice(-lat, lat))
-    dt = dt.sel(xt_ocean=lon_i, yt_ocean=slice(lat_i, lat_f))
-    ds = ds.sel(xt_ocean=lon_i, yt_ocean=slice(lat_i, lat_f))
-
-    Y, Z = np.meshgrid(dt.yt_ocean.values, -dt.st_ocean.values)
-    p = gsw.conversions.p_from_z(Z, Y)
-
-    SA = ds.salt
-    t = dt.temp
-    rho = gsw.pot_rho_t_exact(SA, t, p, p_ref=0)
-    dr = xr.Dataset({'rho': (['Time', 'st_ocean', 'yu_ocean'],  rho - 1000)},
-                    coords={'Time': du.Time,
-                            'st_ocean': du.st_ocean,
-                            'yu_ocean': du.yu_ocean})
-
-    du1 = du.u.where(dr.rho >= rho1, np.nan)
-    du2 = du1.where(dr.rho <= rho2, np.nan)
-    du_euc = du2.where(du.u > 0.1, np.nan)
-
-    return du_euc
-
-
-def EUC_bnds_izumo(du, dt, ds, lon, interpolated=False):
-    """Apply Izumo (2005) EUC definition to zonal velocity at a longitude.
-
-    Izumo (2005):
-        - Zonal velocity (U): U > 0 m s−1,
-        - Depth: 25 m < z < 300 m.
-        - Temperature (T): T < T(z = 15 m) – 0.1°C and T < 27°C
-
-        - Latitudinal boundaries:
-            - Between +/-2° at 25 m,
-            - which linearly increases to  +/-4° at 200 m
-            -via the function 2° – z/100 < y < 2° + z/100,
-            - and remains constant at  +/-4° below 200 m.
-
-    Args:
-        du (Dataset): Zonal velocity dataset.
-        dt (Dataset): Temperature dataset.
-        ds (Dataset): Salinity dataset.
-        lon (float): The EUC longitude examined.
-
-    Returns:
-        du4 (DataArray): The zonal velocity in the EUC region.
-
-    """
-    # Define depth boundary levels.
-    if interpolated:
-        z_15, z1, z2 = 15, 25, 300
-    else:
-        # Modified because this is the correct level for OFAM3 grid.
-        z1 = tools.get_edge_depth(25, index=False)
-        z2 = tools.get_edge_depth(300, index=False)
-        z_15 = 17
-
-    # Find exact latitude longitudes to slice dt and ds.
-    lon_i = dt.xt_ocean[tools.idx(dt.xt_ocean, lon + 0.05)].item()
-
-    # Slice depth and longitude.
-    du = du.sel(xu_ocean=lon, st_ocean=slice(z1, z2), yu_ocean=slice(-4, 4))
-    dt = dt.sel(xt_ocean=lon_i, st_ocean=slice(z1, z2), yt_ocean=slice(-4, 4.1))
-    ds = ds.sel(xt_ocean=lon_i, st_ocean=slice(z1, z2), yt_ocean=slice(-4, 4.1))
-    dt_z15 = dt.temp.sel(st_ocean=z_15, method='nearest')
-    Z = du.st_ocean.values
-
-    y1 = -2 - Z/100
-    y2 = 2 + Z/100
-
-    du1 = du.u.copy().load()
-    du2 = du.u.copy().load()
-
-    for z in range(len(du.st_ocean)):
-        # Remove latitides via function between 25-200 m.
-        if z <= tools.get_edge_depth(200, index=False) - 1:
-            du1[:, z, :] = du.u.isel(st_ocean=z).where(du.yu_ocean > y1[z])
-            du1[:, z, :] = du1.isel(st_ocean=z).where(du.yu_ocean < y2[z])
-
-        # Remove latitides greater than 4deg for depths greater than 200 m.
-        else:
-            du1[:, z, :] = du.u.isel(st_ocean=z).where(du.isel(st_ocean=z).yu_ocean >= -4
-                                                       and du.isel(st_ocean=z).yu_ocean <= 4)
-            # du1[:, z, :] = du1.isel(st_ocean=z).where(du1.yu_ocean <= 4)
-
-        # Remove temperatures less than t(z=15) - 0.1 at each timestep.
-        du2[:, z, :] = du1.isel(st_ocean=z).where(
-            dt.temp.isel(st_ocean=z).values < dt_z15.values - 0.1).values
-
-    # Remove negative/zero velocities.
-    du3 = du2.where(du.u > 0, np.nan)
-
-    # Removed temperatures less than 27C.
-    du4 = du3.where(dt.temp.values < 27)
-    # logger.debug('Izumo depths={:.2f}-{:.2f}'.format(du4.st_ocean[0].item(),
-    #                                                  du4.st_ocean[-1].item()))
-
-    return du4
