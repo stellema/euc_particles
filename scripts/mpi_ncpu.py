00b8b254690f51351f326dfae7457da20a739f2e
stellema <40450353+stellema@users.noreply.github.com>
2020-06-12 12:39:53 +1000

    Update mpi_ncpu.py

--- scripts/mpi_ncpu.py
+++ scripts/mpi_ncpu.py
@@ -70,27 +70,49 @@ def particlefile_vars(filename, lon, add_particles=True):
     return lat, lon, coords
 
 
-def test_ncpu(coords, mpi_size):
+def test_ncpu(mpi_size, coords, lon, coordsx=None, lonx=None, show=True):
     """Test if your ncpu size will work/how it will look by creating partitions like parcels.
 
     This should also give you an idea of how to make your own partitions.
     """
+    psum = False if coordsx is None else True
     kmeans = KMeans(n_clusters=mpi_size, random_state=0).fit(coords)
     partitions = kmeans.labels_
+
+    if psum:
+        kmeans = KMeans(n_clusters=mpi_size, random_state=0).fit(coordsx)
+        partitionsx = kmeans.labels_
+    if show and psum:
+        print('CPUs={}, Particles={} + {} = {}'.format(mpi_size, lon.size, lonx.size,
+                                                       lon.size + lonx.size))
+    elif show and not psum:
+        print('CPUs={}, Particles={}'.format(mpi_size, lon.size))
+
     outcome = 'Success!'
-    print('CPUs={}, Particles={}'.format(mpi_size, lon.size))
     for mpi_rank in range(mpi_size):
-        lonx = lon[partitions == mpi_rank]
-        if lonx.size < mpi_size:
-            print(mpi_rank, lonx.size, 'error')
-            outcome = 'Error :('
-        else:
-            print(mpi_rank, lonx.size)
-    print('MPI size of {}:'.format(mpi_size), outcome)
-    return partitions
+        lonp = lon[partitions == mpi_rank]
+        s = lonp.size
+        if psum:
+            lonxp = lonx[partitionsx == mpi_rank]
+            s = s + lonxp.size
+
+        err = 'Error' if s < mpi_size else ''
+        outcome = 'Error :(' if s < mpi_size else outcome
+        if show and psum:
+            print('{}. {} + {} = {} {}'.format(mpi_rank, lonp.size, lonxp.size,
+                                               lonp.size + lonxp.size, err))
+        elif show and not psum:
+            print('{}. {} {}'.format(mpi_rank, lonp.size, err))
+    if show:
+        print('MPI size of {}:'.format(mpi_size), outcome)
+
+        return partitions
+    else:
+        result = False if outcome == 'Error :(' else True
+        return result
 
 
-def test_cpu_lim(coords, cpu_lim=None):
+def test_cpu_lim(coords, lon, cpu_lim=None, coordsx=None, lonx=None):
     """Find max ncpus.
 
     Each partition must be greater than ncpus (e.g. partition size of 24 when you have
@@ -102,9 +124,8 @@ def test_cpu_lim(coords, cpu_lim=None):
 
     # Check all cpu sizes to see which would work, append the number then print the max size.
     for mpi_size in range(1, cpu_lim + 1):
-        kmeans = KMeans(n_clusters=mpi_size, random_state=0).fit(coords)
-        partitions = kmeans.labels_
-        if all([lon[partitions == rank].size > mpi_size for rank in range(mpi_size)]):
+        works = test_ncpu(mpi_size, coords, lon, coordsx, lonx, show=False)
+        if works:
             ncpu.append(mpi_size)
             print(mpi_size, end=', ')
     print('max ncpu =', max(ncpu))
@@ -120,6 +141,7 @@ if __name__ == "__main__" and cfg.home != Path('E:/'):
     filename = cfg.data/args.filename
     mpi_size = args.mpi_size
     lon = args.lon
+    fieldset = main.ofam_fieldset(time_bnds='full', chunks=300)
 
 else:
     # Lat and lon of particles (whatever goes into your ParticleSet).
@@ -127,7 +149,36 @@ else:
     filename = cfg.data/'sim_190_v0r0.nc'
     mpi_size = 25
     lon = 190
+    fieldset = main.ofam_fieldset(time_bnds='full', chunks=300, time_ext=True, time_periodic=False)
 
-lat, lon, coords = particlefile_vars(filename, lon=lon)
-partitions = test_ncpu(coords, mpi_size=mpi_size)
-ncpu = test_cpu_lim(coords, cpu_lim=52)
+
+p_lats = np.round(np.arange(-2.6, 2.6 + 0.05, 0.1), 2)
+p_depths = np.arange(25, 350 + 20, 25)
+p_lons = np.array([lon])
+
+zParticle = main.get_zParticle(fieldset)
+
+# Add particles on the next day that regularly repeat.
+print('Particlefile particles.')
+psetx = main.particleset_from_particlefile(fieldset, pclass=zParticle, filename=filename,
+                                           restart=True, restarttime=np.nanmin)
+print('EUC particles.')
+pset_start = np.nanmin(psetx.time) - 24*60*60
+pset = main.EUC_pset(fieldset, zParticle, p_lats, p_lons, p_depths,
+                     pset_start, repeatdt=timedelta(days=6))
+
+# print('Adding particles.')
+# pset.add(psetx)
+
+lon = pset.particle_data['lon']
+lat = pset.particle_data['lat']
+coords = np.vstack((lon, lat)).transpose()
+
+lonx = psetx.particle_data['lon']
+latx = psetx.particle_data['lat']
+coordsx = np.vstack((lonx, latx)).transpose()
+
+# lat, lon, coords = particlefile_vars(filename, lon=lon)
+partitionsx = test_ncpu(coords, lon, lonx=lonx, coordsx=coordsx)
+
+ncpu = test_cpu_lim(coords, lon, cpu_lim=50, coordsx=coordsx, lonx=lonx)
