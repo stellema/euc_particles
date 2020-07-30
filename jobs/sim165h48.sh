988a7dc27fb725b53afe58498e503032ae0d80ad
Annette Stellema <astellemas@gmail.com>
2020-07-30 17:34:50 +1000

    gadi

--- jobs/sim165h48.sh
+++ jobs/sim165h48.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=48:00:00
-#PBS -l mem=190GB
+#PBS -l walltime=30:00:00
+#PBS -l mem=192GB
 #PBS -l ncpus=48
 #PBS -l wd
 #PBS -M astellemas@gmail.com
@@ -12,4 +12,4 @@ module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
 module unload openmpi
 module load openmpi/4.0.2
-mpirun -np 48 --report-bindings python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e "hist" -x 165 -r 1098 -v 0 -f "sim_hist_165_v0r0.nc"
+mpirun --report-bindings python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e "hist" -x 165 -r 732 -v 0 -f "sim_hist_165_v0r0.nc"
