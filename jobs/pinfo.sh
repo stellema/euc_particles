30f2b9f0e819ebdbefc9227ac34e8946454fd23a
Stellema <astellemas@gmail.com>
2020-08-20 13:49:04 +1000

    Sim testing info jobs

--- jobs/pinfo.sh
+++ jobs/pinfo.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=15:00:00
-#PBS -l mem=90GB
+#PBS -l walltime=6:00:00
+#PBS -l mem=80GB
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -M astellemas@gmail.com
-#PBS -m abe
+#PBS -m ae
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_190_v0r3.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_165_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_190_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_220_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_165_v0r1.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_190_v0r1.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_190_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_220_v0r2.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_250_v0r2.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v1r0.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v2r0.nc"
--- scripts/sim_info.py
+++ scripts/sim_info.py
@@ -20,10 +20,9 @@ def particle_info(sim_id, info_only=True):
 
     ext = ''
     ds = xr.open_dataset(str(sim_id), decode_cf=False)
-    # Total number of particles
-    start = ds.traj.size
 
-    ds = ds.where(ds.u > 0., drop=True)
+    # Total number of particles
+    start = 159529
     N = ds.traj.size
 
     # Number of intially westward particles.
@@ -41,19 +40,10 @@ def particle_info(sim_id, info_only=True):
         db = np.unique(ds.where(ds.unbeached >= 1, drop=True).trajectory)
         unbeached = db[~np.isnan(db)].size
         ext += ' uB={}({:.1f}%)'.format(unbeached, (unbeached/N)*100)
-    else:
-        ext += ' uB=N/A'
-
-    # Number of beached particles.
-    if hasattr(ds, 'beached'):
-        db = np.unique(ds.where(ds.beached > 3, drop=True).trajectory)
-        beached = db[~np.isnan(db)].size
-        ext += ' B={} max={}'.format(beached, np.nanmax(ds.beached))
-        # Add beached particles %total and %unbeached.
-        if beached > 0:
-            ext += '({:.1f}%N)'.format((beached/N)*100)
-            if hasattr(ds, 'unbeached'):
-                ext += '({:.1f}%uB)'.format((beached/unbeached)*100)
+        dd = np.unique(ds.where(ds.unbeached < 0, drop=True).trajectory)
+        beached = dd[~np.isnan(dd)].size
+        ext += ' B={}({:.1f}%N{:.1f}%uB)'.format(beached, (beached/N)*100,
+                                                 (beached/unbeached)*100)
 
         # Update number of deleted particles minus beached.
         oob = dels - beached
@@ -67,7 +57,6 @@ def particle_info(sim_id, info_only=True):
 
     # Plot some figures!
     main.plot3D(sim_id, ds)
-    main.plot3Dx(sim_id, ds)
 
     return
 
@@ -80,3 +69,29 @@ if __name__ == "__main__":
     file = args.file
     sim_id = cfg.data/file
     particle_info(sim_id, info_only=False)
+
+
+
+# ds = xr.open_dataset(cfg.data/'sim_hist_165_v1r0.nc', decode_cf=False)
+# ds = ds.isel(traj=np.arange(0, 98091, 100))
+# inds = ds.time.argmin(axis=1)
+# ds.lat.isel(obs=inds)
+# ds.zone.isel(obs=inds)
+# end_zone = ds.zone.isel(obs=inds)
+# end_zone.where(end_zone ==10)
+# end_zone.where(end_zone ==10, drop=True)
+
+# end_b = ds.unbeached.isel(obs=inds)
+# end_b
+# end_b.where(end_b < 0, drop=True)
+
+# # R0 number of initial particles (or assume 159529 new).
+# ds.trajectory.max()
+
+
+
+# dz = xr.open_dataset(cfg.data/'sim_hist_165_v2r0.nc', decode_cf=False)
+# inde = dz.time.argmin(axis=1)
+# dze = dz.isel(obs=inde)
+# dzu = dze.where(dze.unbeached < 0, drop=True)
+# indz = dzu.unbeached.argmin(axis=1) # 178
