bfc5248350c90c877ef601db69bded029b9c27b7
Stellema <astellemas@gmail.com>
2020-08-22 18:24:09 +1000

    Updated jobs

---
+++ jobs/v1/pinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normal
+#PBS -l walltime=2:00:00
+#PBS -l mem=30GB
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -M astellemas@gmail.com
+#PBS -m ae
+#PBS -l storage=gdata/hh5+gdata/e14
+module use /g/data3/hh5/public/modules
+module load conda/analysis3-20.04
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_t4r0.nc"
--- jobs/v1/sim165h.sh
+++ jobs/v1/sim165h.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=8:00:00
+#PBS -l walltime=8:30:00
 #PBS -l mem=192GB
 #PBS -l ncpus=48
 #PBS -l wd
@@ -16,3 +16,4 @@ module load openmpi/4.0.2
 
 EXP="hist"
 mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x 165 -r 1296 -v 5
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v5r0.nc"
