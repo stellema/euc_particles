9b664388a1542f1f84aa296090baac8343117c88
Annette Stellema <astellemas@gmail.com>
2020-08-22 18:05:49 +1000

    t4 Figure

--- jobs/v0/pinfo.sh
+++ jobs/v0/pinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=3:00:00
+#PBS -l walltime=2:00:00
 #PBS -l mem=30GB
 #PBS -l ncpus=1
 #PBS -l wd
@@ -10,4 +10,4 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v3r0.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_t4r0.nc"
