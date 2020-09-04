bb4fd0c27e9523059fa93019415f56b6f14a89ab
Stellema <astellemas@gmail.com>
2020-09-04 12:01:02 +1000

    Init simT6

--- jobs/v1/sim165h.sh
+++ jobs/v1/sim165h.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=8:30:00
+#PBS -l walltime=12:00:00
 #PBS -l mem=192GB
 #PBS -l ncpus=48
 #PBS -l wd
@@ -15,5 +15,5 @@ module unload openmpi
 module load openmpi/4.0.2
 
 EXP="hist"
-mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x 165 -r 1296 -v 5
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v5r0.nc"
+mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x 165 -r 1296 -v 6
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v6r0.nc"
