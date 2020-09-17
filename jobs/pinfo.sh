ceb8bceb18bcdbcbe9915f0244d503e3ecaa967d
Annette Stellema <astellemas@gmail.com>
2020-09-17 21:27:05 +1000

    Partially finished v00r0 jobs

--- jobs/pinfo.sh
+++ jobs/pinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=3:00:00
+#PBS -l walltime=5:00:00
 #PBS -l mem=30GB
 #PBS -l ncpus=1
 #PBS -l wd
@@ -10,4 +10,6 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v1r0.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_250_v00r0.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_220_v00r0.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v00r0.nc"
