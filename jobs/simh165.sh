88164fb2c21d8d5eb73357f46732770bd553afb4
Annette Stellema <astellemas@gmail.com>
2020-09-18 17:24:56 +1000

    testing self submitting jobs

--- jobs/simh165.sh
+++ jobs/simh165.sh
@@ -10,51 +10,8 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 #PBS -v NJOBS,NJOB
 #PBS -l wd
-  
-  
-# =============================================================================
-#  Self resubmitting PBS bash script:
-#
-#  * Submits a followon job after executing the current job. Hence it assumes
-#    the program will complete within the requested time to allow time for
-#    the resubmission
-#
-#  * Assumes program being run is checkpointing at regular intervals and is
-#    able to resume execution from a checkpoint
-#
-#  * Uses an environment variable (NJOBS) to limit the total number of 
-#    resubmissions in the sequence of jobs.
-#
-#  * Allows the early termination of the sequence of jobs - just create/touch
-#    the file STOP_SEQUENCE in the jobs working directory.  This may be done 
-#    by the executable program when it has completed the "whole" job or by hand 
-#    if there is a problem
-#
-#  * This script may be renamed anything (<15 characters) but if you use the -N 
-#    option to qsub you must edit the qsub line below to give the script name 
-#    explicitly
-#
-#  * To use: 
-#         - make appropriate changes to the PBS options above and to the 
-#           execution and file manipulation lines belo
-#         - submit the job with the appropriate value of NJOBS, eg:
-#                    qsub -v NJOBS=5 <scriptname>
-#
-#  * To kill a job sequence, either touch the file STOP_SEQUENCE or qdel
-#    the running job (assuming it isnt resubmitting very fast and you can
-#    "catch" one to kill).
-#
-#  * To test, try  "sleep 30"  as your executable line
-#
-# ===============================================================================
 
 ECHO=/bin/echo
-
-#
-# These variables are assumed to be set:
-#   NJOBS is the total number of jobs in a sequence of jobs (defaults to 1)
-#   NJOB is the number of the current job in the sequence (defaults to 1)
-#
   
 if [ X$NJOBS == X ]; then
     $ECHO "NJOBS (total number of jobs in sequence) is not set - defaulting to 1"
@@ -74,21 +31,15 @@ if [ -f STOP_SEQUENCE ] ; then
     exit 0
 fi
 
-#
-# Pre-job file manipulation goes here ...
-# 
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
 module unload openmpi
 module load openmpi/4.0.2
 EXP="hist"
 FILE1="sim_hist_165_v0r00.nc"
-#
-# now run the job
-#
-
 # mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x 165 -r 780 -v 0 -f $FILE1
 sleep 30
+
 # 
 # Check the exit status
 #
@@ -107,18 +58,12 @@ fi
 #   
 if [ $NJOB -lt $NJOBS ]; then
 
-# 
-# Post-job file manipulation (preparing for next job etc) goes here ...
-#
-# INSERT CODE HERE
-#
-  
 #
 # Now increment counter and submit the next job
 # 
     NJOB=$(($NJOB+1))
     $ECHO "Submitting job number $NJOB in sequence of $NJOBS jobs"
-    qsub run_jobs
+    qsub -v NJOBS=5 /g/data/e14/as3189/OFAM/jobs/simh165.sh
 else
     $ECHO "Finished last job in sequence of $NJOBS jobs"
 fi
