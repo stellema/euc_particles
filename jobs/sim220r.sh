18b19e05fd45ff1fcccc6ec5edb432cb34fc2e84
Stellema <astellemas@gmail.com>
2020-09-18 19:50:03 +1000

    Updated some jobs

--- jobs/sim220r.sh
+++ jobs/sim220r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=40:00:00
+#PBS -l walltime=48:00:00
 #PBS -l mem=192GB
 #PBS -l ncpus=48
 #PBS -l wd
@@ -15,4 +15,6 @@ module unload openmpi
 module load openmpi/4.0.2
 
 EXP="rcp"
-mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x 220 -r 972 -v 0
+LON=220
+FILE="sim_${EXP}_${LON}_v0r00.nc"
+mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x $LON -r 252 -v 0 -f $FILE
