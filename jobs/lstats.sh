9bb050dc3686e61cb833e409b975ac4bef85b407
Annette Stellema <astellemas@gmail.com>
2020-09-19 18:52:27 +1000

    gadi

--- jobs/lstats.sh
+++ jobs/lstats.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=1:00:00
-#PBS -l mem=25GB
+#PBS -l walltime=00:30:00
+#PBS -l mem=15GB
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -M astellemas@gmail.com
@@ -10,6 +10,4 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_165_v0r00.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_190_v0r00.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_220_v0r00.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v0r01.nc"
