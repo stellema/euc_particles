0cad0f559cff8f93a8932b5939aab33f3b286b06
Annette Stellema <astellemas@gmail.com>
2020-09-28 13:53:05 +1000

    gadi

--- jobs/lstats.sh
+++ jobs/lstats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=01:00:00
+#PBS -l walltime=02:00:00
 #PBS -l mem=15GB
 #PBS -l ncpus=1
 #PBS -l wd
@@ -10,9 +10,13 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.04
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_165_v0r01.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_190_v0r01.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_165_v0r02.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_190_v0r02.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_220_v0r02.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_190_v0r02.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_220_v0r03.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_250_v0r03.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_220_v0r03.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v0r03.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_250_v0r04.nc"
 python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v0r04.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_hist_250_v0r05.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v0r05.nc"
+python3 /g/data/e14/as3189/OFAM/scripts/sim_info.py -f "sim_rcp_250_v0r06.nc"
