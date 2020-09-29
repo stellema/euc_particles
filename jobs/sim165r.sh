e35c0458569cf7b46396927a41ad023b06d58a61
Annette Stellema <astellemas@gmail.com>
2020-09-29 11:40:57 +1000

    gadi cancelled jobs

--- jobs/sim165r.sh
+++ jobs/sim165r.sh
@@ -17,5 +17,4 @@ module load openmpi/4.0.2
 EXP="rcp"
 LON=165
 FILE="sim_${EXP}_${LON}_v0r00.nc"
-python3 /g/data/e14/as3189/OFAM/scripts/sim_particleset.py -e $EXP -x $LON -r 0 -v 0
-mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x $LON -r 696 -v 0 -f $FILE
+mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x $LON -r 696 -v 0 -f $FILE -t "out-JLBIOMUC"
