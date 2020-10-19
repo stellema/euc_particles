a4ea625769a5bc756ca5fd3e39a28c5c27f4c75a
Stellema <astellemas@gmail.com>
2020-10-20 00:10:46 +1100

    Updated job conda version

--- jobs/sim250h.sh
+++ jobs/sim250h.sh
@@ -10,11 +10,8 @@
 #PBS -M astellemas@gmail.com
 
 module use /g/data3/hh5/public/modules
-module load conda/analysis3-20.07
-module unload openmpi
-module load openmpi/4.0.2
+module load conda/analysis3-20.10
 
 EXP="hist"
 LON=250
-python3 /g/data/e14/as3189/OFAM/scripts/sim_particleset.py -e $EXP -x $LON -r 780 -v 0
-mpirun python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x $LON -r 780 -v 0 -f True
+mpirun -np 48 python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x $LON -r 780 -v 0 -f True
