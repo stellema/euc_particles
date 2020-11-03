f143a37b230e5f9cd5cd7c8dcf4472b19a3562fa
Stellema <astellemas@gmail.com>
2020-11-03 14:36:20 +1100

    Archived sim jobs, added plx jobs

---
+++ jobs/plx1r.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normal
+#PBS -l walltime=48:00:00
+#PBS -l mem=192GB
+#PBS -l ncpus=48
+#PBS -l storage=gdata/hh5+gdata/e14
+#PBS -l wd
+#PBS -m ae
+#PBS -M astellemas@gmail.com
+
+module use /g/data3/hh5/public/modules
+module load conda/analysis3-20.10
+
+EXP="rcp"
+LON=190
+mpirun -np 48 python3 /g/data/e14/as3189/OFAM/scripts/plx.py -e $EXP -x $LON -r 1200 -v 0 -f 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_${EXP}_${LON}_v0r00.nc"
--- jobs/plx3r.sh
+++ jobs/plx3r.sh
@@ -5,7 +5,6 @@
 #PBS -l mem=192GB
 #PBS -l ncpus=48
 #PBS -l storage=gdata/hh5+gdata/e14
-#PBS -l jobfs=4GB
 #PBS -l wd
 #PBS -m ae
 #PBS -M astellemas@gmail.com
@@ -13,6 +12,7 @@
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.10
 
-EXP="hist"
-LON=165
-mpirun -np 48 python3 /g/data/e14/as3189/OFAM/scripts/sim.py -e $EXP -x $LON -r 1200 -v 0 -f 0
+EXP="rcp"
+LON=250
+mpirun -np 48 python3 /g/data/e14/as3189/OFAM/scripts/plx.py -e $EXP -x $LON -r 1200 -v 0 -f 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_${EXP}_${LON}_v0r00.nc"
