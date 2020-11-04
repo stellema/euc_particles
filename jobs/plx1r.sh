9b8fb8a69d3cd78a475f65604b34c7632ff1726e
Stellema <astellemas@gmail.com>
2020-11-04 23:43:51 +1100

    Updated beach field for plxv1

--- jobs/plx1r.sh
+++ jobs/plx1r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=48:00:00
+#PBS -l walltime=18:00:00
 #PBS -l mem=192GB
 #PBS -l ncpus=48
 #PBS -l storage=gdata/hh5+gdata/e14
@@ -14,5 +14,5 @@ module load conda/analysis3-20.10
 
 EXP="rcp"
 LON=190
-mpirun -np 48 python3 /g/data/e14/as3189/OFAM/scripts/plx.py -e $EXP -x $LON -r 1200 -v 0 -f 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_${EXP}_${LON}_v0r00.nc"
+mpirun -np 48 python3 /g/data/e14/as3189/OFAM/scripts/plx.py -e $EXP -x $LON -r 1200 -v 1 -f 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_${EXP}_${LON}_v1r00.nc"
--- scripts/create_file_unbeach_field.py
+++ scripts/create_file_unbeach_field.py
@@ -36,14 +36,103 @@ vb = np.zeros(ds.u.shape, dtype=np.float32)  # Meridional unbeach direction.
 wb = np.zeros(ds.u.shape, dtype=np.float32)  # Vertical unbeach direction.
 ld = np.zeros(ds.u.shape, dtype=np.float32)  # Land point (1=land,0=ocean).
 
+extra = True
 
 def island(k, j, i):
     """Check A-grid u-cell land point."""
     if all([U[k, jj, ii] == 0 and V[k, jj, ii] == 0
             for jj in [j, j+1] for ii in [i, i+1]]):
-        return True
+        return np.True_
     else:
-        return False
+        return np.False_
+
+
+def islandlocked(k, j, i):
+    """Check if point is landlocked."""
+    return all([island(k, jj, ii) for jj in [j-1, j, j+1] for ii in [i-1, i, i+1]])
+
+
+def extra_unbeach_directions(k, j, i, ub, vb):
+    # Land:W Ocean:E
+    if island(k, j, i-1) and ~island(k, j, i+1):
+        # Land:W+S Ocean:N+E
+        if island(k, j-1, i) and ~island(k, j+1, i):
+            if ~island(k, j-1, i+1):  # Land:W+S Ocean:N+E+SE -> SE
+                ub[0, k, j, i] = 1
+                vb[0, k, j, i] = -1
+            elif ~island(k, j+1, i-1):  # Land:W+S+SE Ocean:E+N+NW -> NW
+                ub[0, k, j, i] = -1
+                vb[0, k, j, i] = 1
+            elif ~island(k, j-1, i-1):  # Land:W+S+SE+NW Ocean:E+N+SW -> SW
+                ub[0, k, j, i] = -1
+                vb[0, k, j, i] = -1
+        # Land:N+W Ocean:S+E
+        elif ~island(k, j-1, i) and island(k, j+1, i):
+            if ~island(k, j+1, i-1):  # Land:N+W Ocean:S+E+NW -> W
+                ub[0, k, j+1, i] = -1
+            if j < lat.size-3 and ~island(k, j+2, i):  # Land:N+W+NW Ocean:S+E+NN -> N
+                vb[0, k, j+1, i] = 1
+    # Land:E Ocean:W
+    elif ~island(k, j, i-1) and island(k, j, i+1):
+        # Land:S+E Ocean:N+W
+        if island(k, j-1, i) and ~island(k, j+1, i):
+            if ~island(k, j-1, i+1):   # Land:S+E Ocean:N+W+SE -> S
+                vb[0, k, j, i+1] = -1
+            if i < lon.size-3 and ~island(k, j, i+2):  # Land:S+E Ocean:N+W+EE -> E
+                ub[0, k, j, i+1] = 1
+        # Land:N+E Ocean:S+W
+        elif ~island(k, j-1, i) and island(k, j+1, i):
+            if i < lon.size-3 and ~island(k, j+1, i+2):  # Land:N+E Ocean:S+W+NEE -> E
+                ub[0, k, j+1, i+1] = 1
+            if j < lat.size-3 and ~island(k, j+2, i+1):  # Land:N+E Ocean:S+W+NNE -> N
+                vb[0, k, j+1, i+1] = 1
+        # Land:E Ocean:N+S+W
+        elif ~island(k, j-1, i) and ~island(k, j+1, i):
+            if i < lon.size-3 and ~island(k, j+1, i+2):   # Land:E Ocean:N+S+W+NEE -> E
+                ub[0, k, j+1, i+1] = 1
+            if j < lat.size-3 and ~island(k, j+2, i+1):  # Land:E Ocean:N+S+W+NNE -> N
+                vb[0, k, j+1, i+1] = 1
+            if i < lon.size-3 and ~island(k, j, i+2):  # Land:E Ocean:N+S+W+EE-> E
+                ub[0, k, j, i+1] = 1
+            if j < lat.size-3 and ~island(k, j-1, i+1):  # Land:E Ocean:N+S+W+SE-> S
+                vb[0, k, j, i+1] = -1
+    # Land:- Ocean:E+W
+    elif ~island(k, j, i-1) and ~island(k, j, i+1):
+        # Land:S Ocean:N+E+W
+        if island(k, j-1, i) and ~island(k, j+1, i):
+            if ~island(k, j-1, i+1):  # Land:S Ocean:N+E+W+SE -> SE
+                ub[0, k, j, i] = 1
+                vb[0, k, j, i] = -1
+                vb[0, k, j, i+1] = -1
+            elif ~island(k, j+1, i-1):  # Land:S Ocean:N+E+W+NW -> NW
+                ub[0, k, j, i] = -1
+                vb[0, k, j, i] = 1
+            elif ~island(k, j-1, i-1):  # Land:S Ocean:N+E+W+SW -> SW
+                ub[0, k, j, i] = -1
+                vb[0, k, j, i] = -1
+            elif i < lon.size-3 and ~island(k, j, i+2):  # Land:S Ocean:N+E+W+ee -> E
+                ub[0, k, j, i+1] = 1
+        # Land:N Ocean:S+E+W
+        elif ~island(k, j-1, i) and island(k, j+1, i):
+            if ~island(k, j+1, i-1):  # Land:N Ocean:S+E+W+NW -> W
+                ub[0, k, j+1, i] = -1
+            if i < lon.size-3 and ~island(k, j+1, i+2):  # Land:N Ocean:S+E+W+NEE -> E
+                ub[0, k, j+1, i+1] = 1
+            if j < lat.size-3 and ~island(k, j+2, i):  # Land:N Ocean:S+E+W+NN -> N
+                vb[0, k, j+1, i] = 1
+            if j < lat.size-3 and ~island(k, j+2, i+1):  # Land:N Ocean:S+E+W+NNE -> N
+                vb[0, k, j+1, i+1] = 1
+        # Land:- Ocean:N+S+E+W
+        elif ~island(k, j-1, i) and ~island(k, j+1, i):
+            if ~island(k, j+1, i-1):  # Land:- Ocean:N+S+E+W+NW -> W
+                ub[0, k, j+1, i] = -1
+            if i < lon.size-3 and ~island(k, j+1, i+2):  # Land:- Ocean:N+S+E+W+NEE -> E
+                ub[0, k, j+1, i+1] = 1
+            if j < lat.size-3 and ~island(k, j+2, i):  # Land:- Ocean:N+S+E+W+NN -> N
+                vb[0, k, j+1, i] = 1
+            if j < lat.size-3 and ~island(k, j+2, i+1):  # Land:- Ocean:N+S+E+W+NNE -> N
+                vb[0, k, j+1, i+1] = 1
+    return ub, vb
 
 
 for k in range(depth.size):
@@ -75,6 +164,8 @@ for k in range(depth.size):
                     if k > 0 and not island(k-1, jj, ii):
                         wb[0, k, jj, ii] = -1
 
+                if extra and not islandlocked(k, j, i):
+                    ub, vb = extra_unbeach_directions(k, j, i, ub, vb)
 
 # Create DataArrays.
 dims = ('Time', 'sw_ocean', 'yu_ocean', 'xu_ocean')
@@ -99,6 +190,27 @@ for g in list(dims):
 
 db.attrs['history'] = 'Created {}.'.format(datetime.now().strftime("%Y-%m-%d"))
 db = db.chunk({'Time': 1, 'sw_ocean': 1, 'yu_ocean': 300, 'xu_ocean': 300})
+xstr = '_x' if extra else ''
 db.to_netcdf(path=cfg.data/'ofam_field_beach.nc')
 print(db, db.Ub.dtype, db.Ub.chunks)
 db.close()
+
+
+# dd = xr.open_dataset(cfg.data/'ofam_field_beach.nc')
+# dx = xr.open_dataset(cfg.data/'ofam_field_beach_x.nc')
+# import matplotlib.pyplot as plt
+
+# fig = plt.figure()
+# dx.Ub[0, 0].plot()
+# dd.Ub[0, 0].where(dx.Ub[0, 0] != dd.Ub[0, 0]).plot()
+
+# dd.Ub[0, 0].plot()
+# dx.Ub[0, 0].plot()
+
+# df = dx.Ub
+# df[0, 20].sel(xu_ocean=slice(128, 160), yu_ocean=slice(-9, -2)).plot.pcolormesh(cmap=plt.cm.seismic)
+# df = dx.Ub.where(dx.Vb != dd.Vb)
+# df[0, 20].sel(xu_ocean=slice(128, 160), yu_ocean=slice(-9, -2)).plot.pcolormesh(cmap=plt.cm.cool)
+
+
+# dx.Ub.where(db.Ub != dd.Ub)[0, 0].plot.pcolormesh(cmap=plt.cm.Wistia)
