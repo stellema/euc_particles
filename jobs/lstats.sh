f1453a22c6a88de4ec705b4d8962e629998333e1
Stellema <astellemas@gmail.com>
2020-11-10 12:33:05 +1100

    Updated particle.log

--- jobs/lstats.sh
+++ jobs/lstats.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -P e14
 #PBS -q normal
-#PBS -l walltime=01:30:00
-#PBS -l mem=15GB
+#PBS -l walltime=02:30:00
+#PBS -l mem=30GB
 #PBS -l ncpus=1
 #PBS -l wd
 #PBS -M astellemas@gmail.com
@@ -10,16 +10,23 @@
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-20.10
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_165_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_190_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_220_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_250_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_165_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_190_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_220_v1r03.nc" -n 0
-python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_250_v1r03.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_165_v1r05.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_190_v1r05.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_220_v1r05.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_250_v1r05.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_165_v1r06.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_190_v1r06.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_hist_220_v1r06.nc" -n 0
 python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_165_v1r04.nc" -n 0
 python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_190_v1r04.nc" -n 0
 python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_220_v1r04.nc" -n 0
 python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_250_v1r04.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_165_v1r06.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_190_v1r06.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_220_v1r06.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_250_v1r06.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_165_v1r07.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_190_v1r07.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_220_v1r07.nc" -n 0
+python3 /g/data/e14/as3189/OFAM/scripts/plx_info.py -f "plx_rcp_250_v1r07.nc" -n 0
 
