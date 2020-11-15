731faad1e530fa75f93a3bd0a03880e5e71e623f
Stellema <astellemas@gmail.com>
2020-11-15 12:14:23 +1100

    Trialling new plx job scripts

---
+++ jobs/plxp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normalbw
+#PBS -l walltime=10:0:00
+#PBS -l mem=100GB
+#PBS -l ncpus=1
+#PBS -l storage=gdata/hh5+gdata/e14
+#PBS -l wd
+#PBS -m ae
+#PBS -M astellemas@gmail.com
+#PBS -v LON,EXP
+ECHO=/bin/echo
+
+module use /g/data3/hh5/public/modules
+module load conda/analysis3-20.10
+python3 /g/data/e14/as3189/OFAM/scripts/plx_particleset.py -e $EXP -x $LON -r 1200 -v 1
+
+# Check the exit status
+errstat=$?
+if [ $errstat -ne 0 ]; then
+    # A brief nap so PBS kills us in normal termination
+    # If execution line above exceeded some limit we want PBS
+    # to kill us hard
+    sleep 5 
+    $ECHO "Job $EXP $LON returned an error status $errstat - stopping job sequence."
+    exit $errstat
+fi
+# Submit parallel job
+$ECHO "Submitting job $EXP $LON"
+qsub -v LON=$LON EXP=$EXP plx.sh
