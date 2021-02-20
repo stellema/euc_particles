46b2636993c00d50118d9e9531dcb199c722d60c
Annette Stellema <astellemas@gmail.com>
2021-02-21 06:41:52 +1100

    EUC monthly velocity anomalies (historical)

--- jobs/u_std_anom.sh
+++ jobs/u_std_anom.sh
@@ -2,12 +2,12 @@
 #PBS -P e14
 #PBS -q normalbw
 #PBS -l walltime=10:00:00
-#PBS -l mem=30GB
-#PBS -l ncpus=2
+#PBS -l mem=72GB
+#PBS -l ncpus=8
 #PBS -l wd
 #PBS -M astellemas@gmail.com
 #PBS -m ae
 #PBS -l storage=gdata/hh5+gdata/e14
 module use /g/data3/hh5/public/modules
 module load conda/analysis3-unstable
-mpirun -np 2 python3 /g/data/e14/as3189/OFAM/scripts/create_file_ofam_EUC_anom_std.py
+mpirun -np 8 python3 /g/data/e14/as3189/OFAM/scripts/create_file_ofam_EUC_anom_std.py
--- scripts/create_file_ofam_EUC_anom_std.py
+++ scripts/create_file_ofam_EUC_anom_std.py
@@ -22,11 +22,14 @@ except ImportError:
 
 rank = MPI.COMM_WORLD.Get_rank() if MPI else 0
 logger = mlogger('misc', parcels=False, misc=True)
-year = [1981, 2012]
+n = 4
+years = np.arange(2070, 2101 + 1, n)
+year = [years[rank], years[rank] + n - 1]
+
 x, y, z = np.arange(140, 295), slice(-5, 5), slice(0, 35)
-clim = xr.open_dataset(cfg.ofam / 'ocean_u_{}-{}_climo.nc'.format(*year))
+clim = xr.open_dataset(cfg.ofam / 'ocean_u_{}-{}_climo.nc'.format(years[0], 2101))
 clim = clim.sel(xu_ocean=x, yu_ocean=y).isel(st_ocean=z)
-new_file = 'ofam_u_month_anom_std_{}-{}.nc'.format(*year)
+new_file = 'ofam_u_month_anom_r{}.nc'.format(rank)
 
 def premeansnip(ds):
     ds = ds.drop(['Time_bounds', 'average_DT', 'nv', 'st_edges_ocean'])
@@ -42,11 +45,10 @@ ds = xr.open_mfdataset(files, combine='by_coords', concat_dim="Time",
                        parallel=parallel)
 
 logger.info("{}: Rank={}: Dataset opened.".format(new_file, rank))
-dsd = ds.std(dim='Time')
-logger.info("{}: Rank={}: Calculated std.".format(new_file, rank))
-dsd = dsd.load()
+#dsd = ds.std(dim='Time')
+#logger.info("{}: Rank={}: Calculated std.".format(new_file, rank))
+#dsd = dsd.load()
 logger.info("{}: Rank={}: Loaded.".format(new_file, rank))
-dsd.to_netcdf(cfg.data / new_file)
+ds.to_netcdf(cfg.data / new_file, compute=True)
 logger.info("{}: Rank={}: Saved.".format(new_file, rank))
-dsd.close()
 ds.close()
