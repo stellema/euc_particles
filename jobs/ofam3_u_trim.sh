ed00d973bbcf017b9fa5976242330767c0039270
Annette Stellema <astellemas@gmail.com>
2021-08-05 11:43:17 +1000

    Add plx_165_hist*.nc compression and OFAM3 zip job scripts and stdout

---
+++ jobs/ofam3_u_trim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -P e14
+#PBS -q normal
+#PBS -l walltime=10:00:00
+#PBS -l mem=8GB
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -l storage=gdata/e14
+module load nco
+module load cdo
+cd /g/data/e14/as3189/OFAM/trop_pac/tape/
+for f in ocean_u_*.nc
+do
+echo "$f"
+ncks -O -d yu_ocean,-14.9,15.0 -d xu_ocean,120.09,295.0 "${f}" "../${f}"
+rm "$f"
+done
