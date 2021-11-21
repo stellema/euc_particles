281305432313f956b2edef1cabd9e6dd670e79e5
Annette Stellema <40450353+stellema@users.noreply.github.com>
2021-11-21 22:36:37 +1100

    Update split_plx_files.py

--- scripts/split_plx_files.py
+++ scripts/split_plx_files.py
@@ -25,29 +25,30 @@ logger = mlogger('misc', parcels=False, misc=True)
 
 def search_combine_plx_datasets(xids, traj):
     """Search plx datasets containing specific particles then combine."""
+    next_obs = 0
     dss = []
     for i, xid in enumerate(xids):
         dx = open_plx_data(xid, chunks='auto')
 
-        try:
-            # Drop last obs (duplicate).
-            dx = dx.isel(obs=slice(0, -1))
+        # try:
+        # Drop last obs (duplicate).
+        dx = dx.isel(obs=slice(0, -1))
 
-            # Reset obs coords (reset to zero & start from last).
-            if i >= 1:
-                next_obs = dss[i - 1].obs.max().item() + 1
-                dx.coords['obs'] = dx.obs - dx.obs[0].item() + next_obs
+        # Reset obs coords (reset to zero & start from last).
+        if i >= 1:
+            dx.coords['obs'] = dx.obs - dx.obs[0].item() + next_obs
 
-            # Subset dataset with particles.
-            dx = dx.where(dx.traj.isin(traj), drop=True)
+        next_obs = dx.obs.max().item() + 1
+        # Subset dataset with particles.
+        dx = dx.where(dx.traj.isin(traj), drop=True)
 
-            # Add list of datasets to be combined.
-            if dx.traj.size >= 1:
-                dss.append(dx)
+        # Add list of datasets to be combined.
+        if dx.traj.size >= 1:
+            dss.append(dx)
 
-        except IndexError:
-            # Pass on datasets that don't contain any of the trajectories.
-            pass
+        # except IndexError:
+        #     # Pass on datasets that don't contain any of the trajectories.
+        #     pass
 
     ds = xr.combine_nested(dss, 'obs', data_vars="minimal", combine_attrs='override')
     return ds
@@ -108,4 +109,4 @@ if __name__ == "__main__":
     p.add_argument('-e', '--exp', default=0, type=int,
                     help='Historical=0 or RCP8.5=1.')
     args = p.parse_args()
-    save_particle_data_by_year(args.lon, args.exp, v=1, r_range=[0, 10])
+    save_particle_data_by_year(args.lon, args.exp, v=1, r_range=[0, 3])
