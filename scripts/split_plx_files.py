db2e597f4c2d89368d9f34b27f34f3a70d9d20b4
Annette Stellema <40450353+stellema@users.noreply.github.com>
2021-11-21 20:18:52 +1100

    Update split_plx_files.py

--- scripts/split_plx_files.py
+++ scripts/split_plx_files.py
@@ -22,13 +22,22 @@ from plx_fncs import (get_plx_id, get_plx_id_year, open_plx_data,
 logger = mlogger('misc', parcels=False, misc=True)
 
 
+
 def search_combine_plx_datasets(xids, traj):
     """Search plx datasets containing specific particles then combine."""
     dss = []
-    for xid in xids:
-        dx = open_plx_data(xid)
+    for i, xid in enumerate(xids):
+        dx = open_plx_data(xid, chunks='auto')
 
         try:
+            # Drop last obs (duplicate).
+            dx = dx.isel(obs=slice(0, -1))
+
+            # Reset obs coords (reset to zero & start from last).
+            if i >= 1:
+                next_obs = dss[i - 1].obs.max().item() + 1
+                dx.coords['obs'] = dx.obs - dx.obs[0].item() + next_obs
+
             # Subset dataset with particles.
             dx = dx.where(dx.traj.isin(traj), drop=True)
 
@@ -47,37 +56,35 @@ def search_combine_plx_datasets(xids, traj):
 def save_particle_data_by_year(lon, exp, v=1, r_range=[0, 10]):
     """Split and save particle data by release year."""
 
-    def open_new_plx_particles(xid):
+    def get_new_plx_particles(xid):
         """Open dataset and return with only initial data."""
-        drop_vars = ['lat', 'lon', 'z', 'zone', 'distance', 'unbeached', 'u']
-        ds = open_plx_data(xid, drop_variables=drop_vars).isel(obs=0, drop=True)
-        ds = ds.where(ds.age == 0, drop=True)
-        return ds
+        ds = open_plx_data(xid, chunks='auto')
+        ds = ds.isel(obs=0, drop=True)
+        ds = ds.where(ds.age == 0., drop=True)
+        traj = ds.trajectory.values.astype(int)
+        ds.close()
+        return traj
 
     name = 'plx_{}_{}_v{}'.format(cfg.exp_abr[exp], lon, v)
     logger.info('Subsetting by year {}.'.format(name))
 
-    y_range = np.arange(cfg.years[exp][-1], cfg.years[exp][0] -1, -1, dtype=int)
-
     xids = [get_plx_id(cfg.exp_abr[exp], lon, v, r) for r in range(*r_range)]
-    xids_new = [get_plx_id_year(cfg.exp_abr[exp], lon, v, r) for r in y_range]
+    xids_new = [get_plx_id_year(cfg.exp_abr[exp], lon, v, r) for r in r_range]
 
     # Subset of merged dataset.
     logger.debug('{}: Opening subset of data.'.format(name))
 
-    dx = xr.combine_nested([open_new_plx_particles(xid) for xid in xids],
-                           'traj', data_vars="minimal", combine_attrs='override')
-    logger.debug('{}: Filter new particles: traj size={}: ...'.format(name, dx.traj.size))
-
-    for i, y in enumerate(y_range):
+    for i, y in enumerate(r_range):
         if not xids_new[i].exists():
-            logger.debug('{}: {}: Filter by year: ...'.format(name, xids_new[i].stem))
-            traj = dx.where(dx['time.year'] == y, drop=True).traj
+
+            logger.debug('{}: {}: Filter by r: ...'.format(name, xids_new[i].stem))
+            traj = get_new_plx_particles(xids[i])
             logger.debug('{}: {}: Filter by year: Success! #traj={}'
                          .format(name, xids_new[i].stem, traj.size))
 
             logger.debug('{}: {}: Search & combine full data: ...'.format(name, xids_new[i].stem))
             ds = search_combine_plx_datasets(xids, traj)
+
             logger.debug('{}: {}: Search & combine full data: Success!'
                          .format(name, xids_new[i].stem))
 
