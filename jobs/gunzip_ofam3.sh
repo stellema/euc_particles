1d2b052bd4a59187cca7f42f812bc605f1320a15
Annette Stellema <40450353+stellema@users.noreply.github.com>
2021-12-01 12:42:58 +1100

    Update gunzip_ofam3.sh

--- jobs/gunzip_ofam3.sh
+++ jobs/gunzip_ofam3.sh
@@ -18,20 +18,20 @@ cd /g/data/e14/as3189/OFAM/trop_pac
 # Historical 1981-2012
 for var in "u" "v" "w"; do
   for year in {1..9}; do
-    gunzip  ocean_"$var"_198"$year"*.nc.gz &
+    gunzip -v ocean_"$var"_198"$year"*.nc.gz &
   done
-  gunzip  ocean_"$var"_199*.nc.gz &
-  gunzip  ocean_"$var"_200*.nc.gz &
+  gunzip -v ocean_"$var"_199*.nc.gz &
+  gunzip -v ocean_"$var"_200*.nc.gz &
   for year in {0..2}; do
-    gunzip  ocean_"$var"_201"$year"*.nc.gz &
+    gunzip -v ocean_"$var"_201"$year"*.nc.gz &
   done
 done
 
 # RCP8.5 2070-2101
 for var in "u" "v" "w"; do
-  gunzip  ocean_"$var"_21*.nc.gz &
+  gunzip -v ocean_"$var"_21*.nc.gz &
   for year in {7..9}; do
-    gunzip  ocean_"$var"_20"$year"*.nc.gz &
+    gunzip -v ocean_"$var"_20"$year"*.nc.gz &
   done
 done
 wait
