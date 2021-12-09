a31a1988f2c5eb7eeaf03bb862b60648fe06088b
Annette Stellema <astellemas@gmail.com>
2021-12-09 18:34:00 +1100

    Create combined particle file & spinup job

--- jobs/spinup.sh
+++ jobs/spinup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-###############################################################################
-#                                                                             #
-#                               Run PLX spinup.                               #
-#                                                                             #
-###############################################################################
 #PBS -P e14
 #PBS -q normal
 #PBS -l walltime=19:00:00
@@ -14,12 +9,18 @@
 #PBS -m ae
 #PBS -M astellemas@gmail.com
 #PBS -v LON,EXP
-cd /g/data/e14/as3189/stellema/plx
+
+###############################################################################
+#
+# Run PLX spinup.
 # To run: qsub -v LON=250,EXP="hist" spinup.sh
+#
+###############################################################################
+cd /g/data/e14/as3189/stellema/plx
 module use /g/data3/hh5/public/modules
 module load conda
 source /g/data/e14/as3189/conda/envs/analysis3-20.01/bin/activate
 
 ECHO=/bin/echo
 $ECHO "Run plx spinup for $EXP at lon $LON."
-mpirun -np 48 python3 /g/data/e14/as3189/stellema/plx/scripts/plx_spinup.py -e $EXP -x $LON -v 1 -r 5 -y 10
+mpirun -np $PBS_NCPUS python3 /g/data/e14/as3189/stellema/plx/scripts/plx_spinup.py -e $EXP -x $LON -v 1 -r 5 -y 6
