f1536a8185fd6280753b9e6bd4dfacad446de67c
Annette Stellema <40450353+stellema@users.noreply.github.com>
2022-09-21 10:25:43 +1000

    Add 2D hist scatter plots

--- scripts/plot_sources.py
+++ scripts/plot_sources.py
@@ -350,6 +350,48 @@ def source_scatter(ds, lon, exp, varx, vary):
     return
 
 
+def source_hist_2d(ds, lon, exp, varx, vary):
+    """Histograms of source variables plot."""
+    if varx == 'u' or vary == 'u':
+        # Convert depth-integrated velocity.
+        ds['u'] = ds['u'] / (25 * 0.1 * cfg.LAT_DEG / 1e6)
+        ds['u'].attrs['long_name'] = 'Velocity'
+        ds['u'].attrs['units'] = 'm/s'
+
+    zn = ds.zone.values
+    fig, axes = plt.subplots(3, 3, figsize=(11.5, 9))
+    axes = axes.flatten()
+
+    i = 0
+    for zi, z in enumerate(zn):
+        zname = ds.names[zi].item()
+        ax = axes[i]
+        dx = ds.sel(zone=z, exp=exp).dropna('traj')
+        x, y = dx[varx], dx[vary]
+        bins='auto'
+        if vary in ['z', 'z_at_zone']:
+            bins = (50, 14)
+        ax = sns.histplot(dx, ax=ax, x=varx, y=vary, log_scale=(0, False),
+                          cmap='plasma', bins=bins, cbar=True,
+                          norm=mpl.colors.LogNorm(), vmin=None, vmax=None)
+
+        ax.set_xlabel('{} [{}]'.format(x.attrs['long_name'], x.attrs['units']))
+        ax.set_ylabel('{} [{}]'.format(y.attrs['long_name'], y.attrs['units']))
+        ax.set_title('{} {}'.format(ltr[i], zname), loc='left', x=-0.01)
+        ax.margins(x=0, y=0)
+        if vary in ['z', 'z_at_zone']:
+            ax.set_ylim(350, 25)
+        if vary in ['u']:
+            ax.axhline(0.1, c='k')
+        i += 1
+
+    plt.tight_layout()
+    plt.savefig(cfg.fig / 'sources/2d_hist_{}_{}_{}_{}.png'
+                .format(varx, vary, lon, cfg.exp_abr[exp]), dpi=300)
+    plt.show()
+    return
+
+
 def plot_KDE_source(ax, ds, var, z, color=None):
     """Plot KDE of source var for historical (solid) and RCP(dashed)."""
     for exp in range(2):
@@ -404,7 +446,6 @@ def log_KDE_source(var):
         for z in [1, 2, 6, 3, 4, 7, 8, 5]:
             log_mode(ds, var, z, lon)
         logger.info('')
-
 
 
 def plot_KDE_multi_var(ds, lon, var):
@@ -489,20 +530,22 @@ def source_KDE_multi_lon(var='z', sum_interior=True):
 # #     combined_source_histogram(ds, lon)
 
 
-for var in ['distance', 'age', 'speed']:
-    source_histogram_multi_lon(var, sum_interior=False)
-    source_KDE_multi_lon(var, sum_interior=True)
-    log_KDE_source(var)
+# for var in ['distance', 'age', 'speed']:
+#     source_histogram_multi_lon(var, sum_interior=False)
+#     source_KDE_multi_lon(var, sum_interior=True)
+#     log_KDE_source(var)
 
-# # for lon in [165]:
-# for lon in cfg.lons:
-#     ds = source_dataset(lon, sum_interior=True)
-#     exp = 0
-#     varx, vary = 'z_at_zone', 'z'
-#     source_scatter(ds, lon, exp, varx, vary)
-#     for vary in ['z', 'u', 'lat']:
-#         varx = 'age'
-#         source_scatter(ds, lon, exp, varx, vary)
+# for lon in [165]:
+for lon in cfg.lons:
+    ds = source_dataset(lon, sum_interior=True)
+    exp = 0
+    varx, vary = 'z_at_zone', 'z'
+    # source_scatter(ds, lon, exp, varx, vary)
+    source_hist_2d(ds, lon, exp, varx, vary)
+    for vary in ['z', 'u', 'lat']:
+        varx = 'age'
+        # source_scatter(ds, lon, exp, varx, vary)
+        source_hist_2d(ds, lon, exp, varx, vary)
 
 ###############################################################################
 # lon = 165
